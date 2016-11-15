694f7badeaa3ff987b37d78ae43b4daf895cd645
Patrick Tchankue <patrick@tangentsolutions.co.za>
2016-11-15 13:01:53 +0200

    main controller

--- SpringWebsite/src/main/java/mil/cm/navy/controller/Home.java
+++ SpringWebsite/src/main/java/mil/cm/navy/controller/Home.java
@@ -1,7 +1,13 @@
 package mil.cm.navy.controller;
 
+import mil.cm.navy.form.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Created by patricktchankue on 11/15/16.
@@ -13,4 +19,16 @@ public class Home {
     public String index(){
         return "index";
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addNewPost(@Valid Post post, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "index";
+        }
+        // model.addAttribute("post", new Post());
+        model.addAttribute("title", post.getTitle());
+        model.addAttribute("content", post.getContent());
+        return "result";
+    }
+
 }
