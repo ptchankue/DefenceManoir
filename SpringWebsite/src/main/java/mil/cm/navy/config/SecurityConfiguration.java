a73b3384f10c1e4668c54f893628874557a44b4e
Patrick Tchankue <patrick@tangentsolutions.co.za>
2016-11-22 20:02:33 +0200

    Fixing all routes to be exempted from athentification

--- SpringWebsite/src/main/java/mil/cm/navy/config/SecurityConfiguration.java
+++ SpringWebsite/src/main/java/mil/cm/navy/config/SecurityConfiguration.java
@@ -3,14 +3,32 @@ package mil.cm.navy.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
- 
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
  
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+        //httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/resources/**", "/webjars/**").permitAll()
+                .antMatchers("/signup", "/about", "/", "/home").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .failureUrl("/login?error")
+                    .defaultSuccessUrl("/")
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+
     }
 
     @Override
@@ -20,4 +38,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("user2").password("secret2").roles("USER");
     }
+
+
 }
