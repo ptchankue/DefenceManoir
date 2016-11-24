2a0cf622f3d0e46c95f7626e8b5836d5b9c4f8a1
Patrick Tchankue <patrick@tangentsolutions.co.za>
2016-11-24 09:26:10 +0200

    Small updates

--- SpringWebsite/src/main/java/mil/cm/navy/controller/LoginController.java
+++ SpringWebsite/src/main/java/mil/cm/navy/controller/LoginController.java
@@ -1,7 +1,14 @@
 package mil.cm.navy.controller;
 
+import mil.cm.navy.Application;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by patricktchankue on 11/15/16.
@@ -9,9 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    private static Logger myLogger;
 
     @RequestMapping("/login")
     public String login(){
+
         return "login/login";
     }
 
