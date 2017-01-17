0dc0b3fd6390fbec51d534a760ffdf593a930ff0
Patrick Tchankue <patrick@tangentsolutions.co.za>
2017-01-17 17:01:11 +0200

    few more updates..

--- SpringWebsite/src/main/java/mil/cm/navy/utils/Response.java
+++ SpringWebsite/src/main/java/mil/cm/navy/utils/Response.java
@@ -4,4 +4,13 @@ package mil.cm.navy.utils;
  * Created by patricktchankue on 12/14/16.
  */
 public class Response {
+    int status;
+    String data;
+    String message;
+
+    public Response(){
+
+    }
+    //convert to json
+
 }
