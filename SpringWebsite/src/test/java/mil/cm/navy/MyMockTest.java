58a47c7d7aafbcb520ad628a18f3a24df3a2e8f7
Patrick Tchankue <patrick@tangentsolutions.co.za>
2017-01-17 16:59:37 +0200

    Adding a few test

--- SpringWebsite/src/test/java/mil/cm/navy/MyMockTest.java
+++ SpringWebsite/src/test/java/mil/cm/navy/MyMockTest.java
@@ -1,7 +1,59 @@
 package mil.cm.navy;
 
+import mil.cm.navy.domain.MyClass;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Iterator;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+
+
 /**
  * Created by patricktchankue on 12/19/16.
  */
 public class MyMockTest {
+
+
+    @Test
+    public void test1()  {
+        //  create mock
+        MyClass test = Mockito.mock(MyClass.class);
+
+        // define return value for method getUniqueId()
+        when(test.getUniqueId()).thenReturn(43);
+
+        // use mock in test....
+        assertEquals(test.getUniqueId(), 43);
+    }
+
+    @Test
+    public void test2()  {
+        //  create mock
+        MyClass test = Mockito.mock(MyClass.class);
+
+        // define return value for method getJsonObject()
+        JSONObject resp = new JSONObject();
+        try{
+            resp.put("message", "My message for  a test5");
+            when(test.getJsonObject()).thenReturn(resp);
+
+            // use mock in test....
+            assertEquals(test.getJsonObject(), resp);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+    }
+    @Test
+    public void test3(){
+        //  create mock
+        MyClass test = Mockito.mock(MyClass.class);
+    }
+
 }
--- SpringWebsite/src/test/java/mil/cm/navy/MyTest.java
+++ SpringWebsite/src/test/java/mil/cm/navy/MyTest.java
@@ -1,7 +1,51 @@
 package mil.cm.navy;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import java.awt.*;
+import java.nio.charset.Charset;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
 /**
  * Created by patricktchankue on 12/12/16.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(Application.class)
+@WebAppConfiguration
 public class MyTest {
+
+    @Value("${server.port}")
+    int port;
+
+    private MockMvc mockMvc;
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception{
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testMe(){
+        //assert(1, 1);
+
+    }
 }
