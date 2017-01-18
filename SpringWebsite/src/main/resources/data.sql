05e1772276065a794d32a581357272329bc8e3b0
Patrick Tchankue <patrick@tangentsolutions.co.za>
2017-01-18 14:09:45 +0200

    Defining more data

--- SpringWebsite/src/main/resources/data.sql
+++ SpringWebsite/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO `country` (`name`) VALUES ('South Africa');
-
 INSERT INTO `country` (`name`) VALUES ('Lesotho');
+INSERT INTO `country` (`name`) VALUES ('Cameroon');
+INSERT INTO `country` (`name`) VALUES ('Namibia');
+INSERT INTO `country` (`name`) VALUES ('Malawi');
+INSERT INTO `country` (`name`) VALUES ('Ethiopia');
+INSERT INTO `country` (`name`) VALUES ('Gabon');
+INSERT INTO `country` (`name`) VALUES ('Zimbabwe');
