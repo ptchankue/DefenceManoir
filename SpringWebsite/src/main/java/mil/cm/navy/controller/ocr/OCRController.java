e075ff065010cf38d15d66c464fedcdf0fbb61c1
patricktchankue <ptchankue@gmail.com>
2017-08-26 16:53:04 +0200

    OCR demo. starting

--- SpringWebsite/src/main/java/mil/cm/navy/controller/ocr/OCRController.java
+++ SpringWebsite/src/main/java/mil/cm/navy/controller/ocr/OCRController.java
@@ -1,7 +1,10 @@
 package mil.cm.navy.controller.ocr;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by patricktchankue on 8/26/17.
  */
+@RequestMapping("ocr")
 public class OCRController {
 }
